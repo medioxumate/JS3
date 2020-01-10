0b12145f0d127c5c01a24d98f4dc69b0ac9dac5e
medioxumate <[briankiehn@comcast.net]>
2020-01-10 17:24:00 -0500

    finished JS3

--- jsreview3.js
+++ jsreview3.js
@@ -1,31 +1,62 @@
 /**
+ * Created in PhpStorm
  * @author Brian Kiehn
- * @version 1.0
+ * @date 1/10/2020
+ * @version 2.0
  * jsreview3.js - review from chapter 3
- * last edited: 4/16/2019
+ * GreenRiverDev
  */
 document.open();
 
-function heehaw(){
-    var output = "<p>";
+let output;
+let MIN;
+let MAX;
 
-    for (let i = 1; i <= 100; i++) {
-        if((i % 3 === 0) && (i % 5 ===0)){
-            output += "Hee Haw!" + "<br>";
+function promp() {
+    let temp;
+
+    temp = prompt("pick the min:");
+    MIN = parseInt(temp, 10);
+
+    temp = prompt("pick the max:");
+    MAX = parseInt(temp, 10);
+
+    if (!isNaN(MIN) && !isNaN(MAX)) {
+        output = "<p>";
+        heeHaw(MIN);
+    }
+    else{
+        if(isNaN(MIN)) {
+            document.write("error: Min "+ MIN+";");
         }
-        else if(i % 3 === 0){
-            output += "Hee!" + "<br>";
-        }
-        else if(i % 5 === 0){
-            output += "Haw!" + "<br>";
-        }
-        else{
-            output += i + "<br>"
+        if(isNaN(MAX)){
+            document.write(" error: Max "+ MAX+"; ");
         }
     }
-    output += "</p>";
-    return output;
 }
 
-document.write(heehaw());
+function heeHaw(out){
+    if(out === MAX+1) {
+        output += "</p>";
+        return output;
+    }
+    else {
+        if(out % 3 === 0 || out % 5 === 0) {
+            if (out % 3 === 0) {
+                output += "Hee! ";
+            }
+            if (out % 5 === 0) {
+                output += "Haw!";
+            }
+        }
+        else {
+            output += out;
+        }
+        output += "<br>";
+        return heeHaw(out+1);
+    }
+}
+
+promp();
+document.write(output);
 document.close();
